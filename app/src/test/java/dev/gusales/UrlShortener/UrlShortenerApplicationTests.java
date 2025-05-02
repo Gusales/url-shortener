bdc1094665ad59dc76b9facc975c6ea3342dcca2
Gusales <dev.gussales@gmail.com>
2025-05-02 10:02:46 -0300

    feat: initial commit
    setup application with java spring boot

---
+++ app/src/test/java/dev/gusales/UrlShortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gusales.UrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
